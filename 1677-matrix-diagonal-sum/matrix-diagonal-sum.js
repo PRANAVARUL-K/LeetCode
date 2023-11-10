370788ccd8d392e76fc26ec4740c71337eb3ed36
PRANAVARUL K <109412173+PRANAVARUL-K@users.noreply.github.com>
2023-11-10 21:35:40 +0530

    Time: 82 ms (5.49%) | Memory: 48.7 MB (5.36%) - LeetSync

---
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var diagonalSum = function(mat) {
+    var one = 0,two = mat.length-1,sum = 0
+    if(mat.length%2 == 1)
+        sum = mat[Math.floor(mat.length/2)][Math.floor(mat.length/2)]
+    else 
+        sum = 0
+    while(one < mat.length && two >= 0)
+    {
+        if(one == two && mat.length%2 == 1)
+        {
+            one++
+            two--
+            continue
+        }
+        console.log(mat[one][one],mat[one][two])
+        console.log(one,two)
+        sum = sum + mat[one][one] + mat[one][two]
+        one++
+        two--
+    }
+    return sum
+};

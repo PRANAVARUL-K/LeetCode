0e69b30f8ea8c930e7d751bb656ae090ea44b4e9
PRANAVARUL K <109412173+PRANAVARUL-K@users.noreply.github.com>
2023-12-01 12:35:45 +0530

    Time: 6 ms (41.64%) | Memory: 12 MB (47.94%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -11,7 +11,28 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if (head == nullptr || k == 0) 
+        if(head==NULL || head->next==NULL||k==0)
+            return head;
+        ListNode* curr=head;
+        int l=0;
+        while(curr->next!=NULL)
+        {
+            curr=curr->next;
+            l++;
+        }
+        curr->next=head;
+        curr = head;
+        k=k%(l+1);
+        int rotate=l-k; 
+        while(rotate!=0)
+        {
+            curr=curr->next;
+            rotate=rotate-1;
+        }
+        ListNode* temp=curr->next;
+        curr->next=NULL;
+        return temp;
+       /* if (head == nullptr || k == 0 || head->next == NULL) 
             return head;
         int length = 1;
         ListNode* temp = head;
@@ -20,9 +41,22 @@ public:
             temp = temp->next;
         }
         ListNode* last = temp;
+        temp->next = head;
         k = k % length; 
         if (k == 0 || length == 1) 
             return head;
+        temp = head;
+        while(k>=0)
+        {
+            last = temp;
+            temp = temp->next;
+            k--;
+        }
+        head = temp;
+        temp = head;
+        while(temp != last)
+            temp = temp->next;        
+        last->next = NULL;
         int val;
         for(int i = 0; i < k; i++)
         {
@@ -38,6 +72,7 @@ public:
             }
             temp->val = val;
         }
+        return head;*/
         return head;
     };
 

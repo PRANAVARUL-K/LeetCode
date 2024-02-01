051518804fd6589dc2b39e1b343964e342e194b6
PRANAVARUL K <109412173+PRANAVARUL-K@users.noreply.github.com>
2024-02-01 14:14:47 +0530

    Time: 33 ms (71.89%) | Memory: 11.7 MB (93.09%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -14,8 +14,6 @@ class Solution(object):
     def checkIfExist(self, arr):
         arr.sort()
         for i in range(len(arr)):
-            '''if(arr[i] == 0 and (i+1 < len(arr) and arr[i+1] == 0)):
-                return True'''
             if(self.Search(arr[i]*2,arr,i)):
                 return True
         return False

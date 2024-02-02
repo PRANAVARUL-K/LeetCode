6062edc272faf867f9da077ea29bc5946fce14ec
PRANAVARUL K <109412173+PRANAVARUL-K@users.noreply.github.com>
2024-02-02 10:02:56 +0530

    Time: 4 ms (86.00%) | Memory: 8.5 MB (9.90%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,14 +3,14 @@ public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length())
             return false;
-        unordered_map<char,int>f;
+        int letters[26] = {0};
         for(int i = 0; s[i] != '\0';i++)
         {
-            f[s[i]]++;
-            f[t[i]]--;
+            letters[t[i]-'a']++;
+            letters[s[i]-'a']--;
         }
-        for(auto i:s)
-            if(f[i]!=0)
+        for(int i = 0; i < 26;i++)
+            if(letters[i]!=0)
                 return false;
         return true;
     }

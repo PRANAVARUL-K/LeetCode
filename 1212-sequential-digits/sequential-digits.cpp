5b41341f37a38aab4dfe63dabdd0067e0fba781e
PRANAVARUL K <109412173+PRANAVARUL-K@users.noreply.github.com>
2024-02-02 14:23:30 +0530

    Time: 2 ms (43.93%) | Memory: 7.4 MB (9.62%) - LeetSync

--- 1212-sequential-digits/sequential-digits.cpp
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -1,21 +1,44 @@
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
+        int a = Digits(low),i,b = Digits(high),k;
+        long long int first = 0,org = 0;
+        long long int add = 0;
+
         vector<int> res;
-        for (int num = 1; num <= 9; ++num) {
-            generateSequentialDigits(num, low, high, res);
+        for(i = 1; i <= a; i++)
+        {
+            first = (first*10)+i;
+            add = add*10 + 1;
         }
-        sort(res.begin(), res.end());
+        org = first;
+        k = i-1;
+        for(i = a;i <= b; i++)
+        {
+            if(first <= high && first >= low)
+                res.push_back(first);
+            while(first%10 != 9)
+            {
+                first = first + add;
+                if(first <= high && first >= low)
+                    res.push_back(first);
+            }
+            k = k+1;
+            first = org*10 + k;
+            org = first;
+            add = add*10 + 1;
+        }
+        sort(res.begin(),res.end());
         return res;
     }
-
-    void generateSequentialDigits(int num, int low, int high, vector<int>& res) {
-        if (num >= low && num <= high) {
-            res.push_back(num);
+    int Digits(int num)
+    {
+        int count = 0;
+        while(num > 0)
+        {
+            count++;
+            num /= 10;
         }
-        int lastDigit = num % 10;
-        if (num > high || lastDigit == 9) return;
-        int nextNum = num * 10 + lastDigit + 1;
-        generateSequentialDigits(nextNum, low, high, res);
+        return count;
     }
 };

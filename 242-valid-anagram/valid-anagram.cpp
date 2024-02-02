fe9fe769b02098f60747c9b3255f9fcf89ae489e
PRANAVARUL K <109412173+PRANAVARUL-K@users.noreply.github.com>
2024-02-02 09:50:35 +0530

    Time: 12 ms (31.79%) | Memory: 9.7 MB (5.23%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -5,21 +5,12 @@ public:
         if(s.length()!=t.length())
             return false;
         for(auto i:s)
-        {
             f[i]++;
-        }
-
         for(auto i:t)
-        {
             f[i]--;
-        }
-
         for(auto i:s)
-        {
             if(f[i]!=0)
                 return false;
-        }
         return true;
-            
     }
 };

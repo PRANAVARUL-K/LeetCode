22f0234b443464543b451fea58744e6069552702
PRANAVARUL K <109412173+PRANAVARUL-K@users.noreply.github.com>
2024-02-10 12:34:37 +0530

    Time: 10 ms (89.33%) | Memory: 20.5 MB (16.35%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) { 
-       int ans=0;
-	   for(auto x:nums)
-	    ans^=x;
-	   return ans;
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int i;
+        for(i = 0; i < nums.size(); i = i+2)
+        {
+            if(i+1 < nums.size() && nums[i] != nums[i+1])
+                return nums[i];
+        }
+        return nums[i-2];
     }
 };

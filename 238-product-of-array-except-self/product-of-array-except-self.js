c67252c211e2a1ca0e3577fe3a1a898560021091
PRANAVARUL K <109412173+PRANAVARUL-K@users.noreply.github.com>
2024-03-15 20:40:18 +0530

    Time: 96 ms (29.52%) | Memory: 59.8 MB (75.57%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    const totalProduct = nums.reduce((acc, num) => acc * num, 1);
+    const countZeros = nums.reduce((count, currentValue) => {
+    if (currentValue === 0) {
+        return count + 1;
+    } else {
+        return count;
+    }
+}, 0);
+const totalProduct0 = nums.reduce((acc, num) => {
+    if(num != 0) 
+        return acc * num;
+        else 
+            return acc;
+        }, 1);
+    return nums.map(num =>{ 
+        if(num == 0 && countZeros == 1) 
+            return totalProduct0
+        else if(countZeros > 1)
+            return 0;
+        else
+            return totalProduct / num
+    });
+};

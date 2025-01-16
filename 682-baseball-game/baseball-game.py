4ee873e1096e3749ea955d966d8ff02c3cac7358
PRANAVARUL K <109412173+PRANAVARUL-K@users.noreply.github.com>
2025-01-16 20:09:20 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (48.06%) - LeetSync

--- 682-baseball-game/baseball-game.py
+++ 682-baseball-game/baseball-game.py
@@ -5,10 +5,14 @@ class Solution:
         for i in operations:
             if i == 'D':
                 res.append(res[top]*2)
+                top = top + 1
             elif i == 'C':
                 res.pop()
+                top = top - 1
             elif i == '+':
                 res.append(res[top]+res[top-1])
+                top = top + 1
             else:
                 res.append(int(i))
+                top = top + 1
         return sum(res)
